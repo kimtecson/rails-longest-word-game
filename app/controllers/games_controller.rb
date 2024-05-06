a952e889e099260f4fadbebd1bca95b7483d22ea
kimberly tecson <tecsonkim@gmail.com>
2024-05-06 16:16:38 +0900

    longest word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters =  ['A','E','I','O','U']
+    @letters += Array.new(5) { (('A'..'Z').to_a - @letters).sample }
+    @letters.shuffle!
+  end
+  def score
+    @word = params[:word].upcase
+    @letters = params[:letters]
+
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+
+    words = JSON.parse(URI.open(url).read)
+    letter_count = @word.chars.uniq.all? do |element|
+      @letters.count(element) >= @word.count(element)
+    end
+
+    raise
+    if words['found'] && letter_count
+      @results = "<b>Congratulations!</b> #{@word} is a valid English word!".html_safe
+    elsif words['found']
+      @results = "Sorry but <b>#{@word}</b> can't be built out of #{@letters.split.join(',')}".html_safe
+    else
+      @results = "Sorry but <b>#{@letters}</b> does not seem to be a valid English word...".html_safe
+    end
+  end
+end
